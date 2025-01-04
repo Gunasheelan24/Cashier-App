7af492446df9a4c6ad64d138294c7799603a878e
GunaSheelan A <140253087+Gunasheelan24@users.noreply.github.com>
2025-01-04 21:49:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ createRoot(document.getElementById('root')).render(
      {/* <Index /> */}
      {/* <Page /> */}
      {/* <PageOne /> */}
-     <PageThree />
+     <PageThree /> 
+    
   </StrictMode>,
 )
